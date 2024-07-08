9c3cf109131e7c89f4cab69b930270709ddb7d3d
MeluXina user <u101957@login02.meluxina.lxp.lu>
2024-07-08 11:46:47 +0200

    changes in iversion

--- inversion/plotter.py
+++ inversion/plotter.py
@@ -199,3 +199,48 @@ def online_pert_plot(packsample, invsample, pert_sample, crop=[0,-1,0,-1], mem_i
             print(f"unable to save figure: {figname}")
         plt.close()
         return
+
+
+        def online_inv_plot_2(packsample, invsample, crop=[0,-1,0,-1], mem_idx=0, figtitle=" ", figname="inv.png"):
+
+        fig = plt.figure(figsize=(15,15))
+
+        #### u
+        vmin = np.min([np.min(packsample[:,0,crop[0]:crop[1],crop[2]:crop[3]])])
+        vmax = np.min([np.max(packsample[:,0,crop[0]:crop[1],crop[2]:crop[3]])])
+
+        ax = fig.add_subplot(331)
+        ax.set_title("u real")
+        im = ax.imshow(packsample[mem_idx,0,crop[0]:crop[1],crop[2]:crop[3]], clim=(vmin, vmax), origin="lower")
+        fig.colorbar(im, shrink=0.5)
+
+
+        #### v
+        vmin = np.min([np.min(packsample[:,1,crop[0]:crop[1],crop[2]:crop[3]])])
+        vmax = np.min([np.max(packsample[:,1,crop[0]:crop[1],crop[2]:crop[3]])])
+
+        ax = fig.add_subplot(332)
+        ax.set_title("v real")
+        im = ax.imshow(packsample[mem_idx,1,crop[0]:crop[1],crop[2]:crop[3]], clim=(vmin, vmax), origin="lower")
+        fig.colorbar(im, shrink=0.5)
+
+  
+
+        #### t2m
+        vmin = np.min([np.min(packsample[:,2,crop[0]:crop[1],crop[2]:crop[3]])])
+        vmax = np.min([np.max(packsample[:,2,crop[0]:crop[1],crop[2]:crop[3]])])
+
+        ax = fig.add_subplot(333)
+        ax.set_title("t2m real")
+        im = ax.imshow(packsample[mem_idx,2,crop[0]:crop[1],crop[2]:crop[3]], clim=(vmin, vmax), origin="lower", cmap="coolwarm")
+        fig.colorbar(im, shrink=0.5)
+
+
+        fig.suptitle(figtitle)
+        fig.tight_layout()
+        try:
+            fig.savefig(figname, dpi=100)
+        except Exception:
+            print(f"unable to save figure: {figname}")
+        plt.close()
+        return
