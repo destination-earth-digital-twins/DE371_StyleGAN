ecff4cc544e1ac4faefe3ceddaecb66c52e26584
Victor <victor.sanchez@meteo.fr>
2024-12-10 17:13:42 +0100

    cleared

--- gan_2_ae/on_belenos/request_and_launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "$0 executed on $(hostname)"
-ARGS=$1
-ARGS2="${ARGS//|/ }"
-cd SAVE
-srun --partition=normal256 --job-name=pre_proc_for_AE --nodes=1 --ntasks=1 --cpus-per-task=1 --time=01:00:00 python3 -u pre_proc_for_AE.py $ARGS2
