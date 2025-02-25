c62e4deb9e8fc2c88e8ec7ddf0d9c6ae72229f83
sanchezvvictor <victor.sanchez@meteo.fr>
2025-02-25 11:20:42 +0100

    main_perturbation corresponding to temporal scale tune done

--- main_perturbation_temporal.py
+++ main_perturbation_temporal.py
@@ -13,7 +13,6 @@ import torch
 import argparse
 import os
 import numpy as np
-import pickle
 import pandas as pd
 from collections import OrderedDict
 from gan.model.stylegan2 import Generator
@@ -21,7 +20,7 @@ from ast import literal_eval as make_tuple
 import utils.utils as utils
 import perturbation.smpca as smpca
 from shutil import copyfile
-from inversion.plotter import online_pert_plot, online_pert_diff_plot
+from inversion.plotter import online_pert_plot
 
 
 def str2list(li):
@@ -41,14 +40,16 @@ def compute_generate_save(G, params, metrics_list, Means, Mins, Maxs, apply_log_
 
 
     N_samples = params.N_samples
-
+    gen = None
     for lt_id, lt in enumerate(params.leadtimes):
-        Ens_r = torch.tensor(np.load(params.pack_dir+f'Rsemble_{datename}_{lt}.npy'), dtype = torch.float32)
+        Ens_r = torch.tensor(np.load(params.pack_dir+f'Rsemble_{params.date_index}_{lt}.npy'), dtype = torch.float32)
         w_ens = torch.tensor(np.load(params.data_dir + f'w_{params.date_index}_{lt}_{params.inv_step}.npy').astype(np.float32))
         inv_ens=np.load(params.data_dir + f'invertFsemble_{params.date_index}_{lt}_{params.inv_step}.npy').astype(np.float32)
-        Ens_feature=None
-        if params.feature_insertion:
-            Ens_feature=torch.tensor(np.load(params.data_dir + f'feature_{params.date_index}_{lt}_{params.inv_step}.npy').astype(np.float32))
+        
+        Ens_r_next = torch.tensor(np.load(params.pack_dir+f'Rsemble_{params.date_index}_{lt+params.dt}.npy'), dtype = torch.float32)
+        w_ens_next = torch.tensor(np.load(params.data_dir + f'w_{params.date_index}_{lt+params.dt}_{params.inv_step}.npy').astype(np.float32))
+        inv_ens_next=np.load(params.data_dir + f'invertFsemble_{params.date_index}_{lt+params.dt}_{params.inv_step}.npy').astype(np.float32)
+
         # subsampling if N_conditioners is lower than initial ensemble size
         if (params.N_conditioners<w_ens.shape[0]):
             cond_indices = np.random.choice(range(w_ens.shape[0]), params.N_conditioners)
@@ -59,19 +60,16 @@ def compute_generate_save(G, params, metrics_list, Means, Mins, Maxs, apply_log_
         Whitening = torch.load(params.eigendir + 'Whitening.pt') if params.sample_rule=='stochastic' else None
         Coloring = torch.load(params.eigendir + 'Coloring.pt') if params.sample_rule=='stochastic' else None
         w0 = torch.load(params.eigendir + 'latent_mean.pt') if params.sample_rule=='stochastic' else None
-        betas = torch.tensor(np.load(os.path.join(params.scale_dir,"ema_scale.npy")).astype(np.float32)[params.scale_interp_step], device=params.device)
-        alphas = torch.tensor(np.load(os.path.join(params.scale_dir,"ema_interp.npy")).astype(np.float32)[params.scale_interp_step], device=params.device)
+        betas = torch.tensor(np.load(os.path.join(params.scale_dir_init,"ema_scale.npy")).astype(np.float32)[params.scale_interp_step], device=params.device)
+        alphas = torch.tensor(np.load(os.path.join(params.scale_dir_init,"ema_interp.npy")).astype(np.float32)[params.scale_interp_step], device=params.device)
+
+        gammas = torch.tensor(np.load(os.path.join(params.scale_dir,"ema_scale.npy")).astype(np.float32)[params.scale_interp_step], device=params.device)
+        thetas = torch.tensor(np.load(os.path.join(params.scale_dir,"ema_interp.npy")).astype(np.float32)[params.scale_interp_step], device=params.device)
 
         title = f'{params.date_index}_{lt}_{params.inv_step}_{params.N_conditioners}'
 
-        path_perturbation=None
-
-        if params.import_perturbation or params.temporal_consistency:
-            title_fixed_perturbation = f'{params.date_index}_{params.initial_timestep}_{params.inv_step}_{params.N_conditioners}'
-            path_perturbation = params.dir_perturbation + f'/samples/perturbation_{title_fixed_perturbation}.npy'
-            print(f'Importing perturbation from perturbation_{title_fixed_perturbation}')
         if lt_id == 0 :
-            gen, w_new = smpca.sm_pca(
+            gen, _ = smpca.sm_pca(
                 Ens_w=w_ens, 
                 G=G, 
                 N_samples=N_samples, 
@@ -85,26 +83,28 @@ def compute_generate_save(G, params, metrics_list, Means, Mins, Maxs, apply_log_
                 Coloring=Coloring,
                 w0=w0
             )
-        gen_next = None # Add function that generates it by reproducing the function in scale_tune_temporal
+        else :
+            gen_next, _ = smpca.sm_pca_temporal(
+                Ens_w=w_ens,
+                Ens_w_next=w_ens_next,
+                G=G, 
+                N_samples=N_samples, 
+                sm_ind=params.style_indices,
+                device=params.device, 
+                sample_rule=params.sample_rule, 
+                thetas=thetas,
+                gammas=gammas,
+                verbose=params.verbose,
+                Whitening=Whitening,
+                Coloring=Coloring,
+                w0=w0
+            )
+        
 
         if params.verbose:
             print(gen.mean(axis=(0,-2,-1)))
             print(Ens_r.mean(axis=(0,-2,-1)))
         
-        
-        
-        if params.save_w_perturbated:
-            if params.save_perturbation and not params.import_perturbation :
-                title+='_generated_pert'
-                np.save(params.output_dir + f'/samples/w_pert_{title}.npy', w_new[0])
-            elif params.import_perturbation:
-                title+='_imported_pert'
-                np.save(params.output_dir + f'/samples/w_pert_{title}.npy', w_new)
-            else :
-                title+='_generated_pert'
-                np.save(params.output_dir + f'/samples/w_pert_{title}.npy', w_new)
-        if params.save_perturbation and not params.import_perturbation :
-            np.save(params.output_dir + f'/samples/perturbation_{title}.npy', w_new[1])
 
         if params.import_normalized_data:
             Ens_r_denorm = utils.denormalize(
@@ -123,6 +123,23 @@ def compute_generate_save(G, params, metrics_list, Means, Mins, Maxs, apply_log_
                                             Maxs=Maxs,
                                             apply_log_transform=apply_log_transform
                                             )
+            if lt_id > 0 :
+                Ens_r_next_denorm = utils.denormalize(
+                                                data=Ens_r_next,
+                                                normalization_type=params.normalization,
+                                                Means=Means,
+                                                Mins=Mins,
+                                                Maxs=Maxs,
+                                                apply_log_transform=apply_log_transform
+                                                )
+                inv_ens_next_denorm = utils.denormalize(
+                                                data=inv_ens_next,
+                                                normalization_type=params.normalization,
+                                                Means=Means,
+                                                Mins=Mins,
+                                                Maxs=Maxs,
+                                                apply_log_transform=apply_log_transform
+                                                )
         gen_denorm = utils.denormalize(
                                         data=gen,
                                         normalization_type=params.normalization,
@@ -131,11 +148,26 @@ def compute_generate_save(G, params, metrics_list, Means, Mins, Maxs, apply_log_
                                         Maxs=Maxs,
                                         apply_log_transform=apply_log_transform
                                         )
+        if lt_id > 0 :
+            gen_next_denorm = utils.denormalize(
+                                            data=gen_next,
+                                            normalization_type=params.normalization,
+                                            Means=Means,
+                                            Mins=Mins,
+                                            Maxs=Maxs,
+                                            apply_log_transform=apply_log_transform
+                                            )
         
         if params.save_normalized_sample:
-            np.save(params.output_dir + f'/samples/genFsemble_{title}.npy', gen)
+            if lt_id == 0 :
+                np.save(params.output_dir + f'/samples/genFsemble_{title}.npy', gen)
+            else :
+                np.save(params.output_dir + f'/samples/genFsemble_{title}.npy', gen_next)
         else:
-            np.save(params.output_dir + f'/samples/genFsemble_{title}.npy', gen_denorm)
+            if lt_id==0:
+                np.save(params.output_dir + f'/samples/genFsemble_{title}.npy', gen_denorm)
+            else :
+                np.save(params.output_dir + f'/samples/genFsemble_{title}.npy', gen_next_denorm)
         
 
         online_pert_plot(
@@ -147,15 +179,19 @@ def compute_generate_save(G, params, metrics_list, Means, Mins, Maxs, apply_log_
             figtitle=f"Generated samples for {title}", 
             figname=params.output_dir + f"/samples/genFsemble_{title}.png"
         )
-
-        online_pert_diff_plot(
-            invsample=inv_ens_denorm, 
-            pert_sample=gen_denorm,
-            crop=[0,-1,0,-1],
-            mem_idx=0 if params.N_conditioners>1 else cond_indices, 
-            figtitle=f"Generated samples for {title}", 
-            figname=params.output_dir + f"/samples/diff_genFsemble_{title}.png"
-        )
+        if lt_id > 0 :
+            online_pert_plot(
+                packsample=Ens_r_next_denorm.numpy() - Ens_r_denorm.numpy(), 
+                invsample=inv_ens_next_denorm - inv_ens_denorm, 
+                pert_sample=gen_next_denorm - gen_denorm,
+                crop=[0,-1,0,-1],
+                mem_idx=0 if params.N_conditioners>1 else cond_indices, 
+                figtitle=f"Generated samples for {title}", 
+                figname=params.output_dir + f"/samples/tempDiffgenFsemble_{title}.png",
+                colormap_var=['RdYlGn','RdYlGn','RdYlGn'],
+                clim_global=(-10,10),
+                axis_title_global='delta'
+            )
 
         if params.runtime_metrics:
             dic = {'Mean' : {'real':Ens_r_denorm.mean(axis=(0,-2,-1)), 'fake':gen_denorm.mean(axis=(0,-2,-1))},
@@ -165,7 +201,11 @@ def compute_generate_save(G, params, metrics_list, Means, Mins, Maxs, apply_log_
             }
             if params.verbose: print(dic)
             return dic
-    
+
+        # Update rule (only doing this for plottinh temporal difference)
+        if lt_id>0 :
+            gen = gen_next.copy()
+
     return {}
      
 if __name__=="__main__" :
@@ -191,12 +231,6 @@ if __name__=="__main__" :
     parser.add_argument('--add_name',type = str, default='')
     parser.add_argument('--eigendir',type = str, default ='')
     
-    # Feature incorporation
-    # See https://arxiv.org/pdf/2202.02183 for more info on Feature insertion
-    parser.add_argument('--feature_insertion', action="store_true", help='Whether to insert the predicted features into the generator.')
-    parser.add_argument('--feature_scale', type=float, default=1, help='Scale of inserted features')
-    parser.add_argument('--feature_id', type=int, default=6, choices=[0,1,2,3,4,5,6,7,8,9,10,11,12,13], help='id of feature to insert')
-    
     # Dataset information
     parser.add_argument("--normalization", type=str, default="meanmax", choices=["minmax", "meanmax", ""])
     parser.add_argument('--max_file', type=str, default='') # use 'MaxNew_4_var.npy' if AROME data # max_rr_log.npy
@@ -209,27 +243,19 @@ if __name__=="__main__" :
     parser.add_argument("--N_samples", type=int, default=10, help='number of new samples') 
     parser.add_argument("--N_conditioners",type=int, default=16, help="number of 'seed' samples used for conditioning")
     parser.add_argument("--inv_step", type=int, default=2000, help='step of inversion to load w')
+    parser.add_argument("--dt", type=int, default=3, help='temporal_difference_step')
     
     
     ######################## PERTURBATION PARAMETERS #######################
     parser.add_argument('--sample_rule', type=str, default='stochastic', 
-                        choices = ['stochastic', 'extrapolation'])
+                        choices = ['stochastic'])
 
     parser.add_argument('--style_indices', type = str2list, default='[1,1,1,1,1,1,1,1,1,1,0,0,0,0]')
     parser.add_argument('--unbias', action="store_true")
 
-    parser.add_argument('--scale_dir', type=str, default="")
+    parser.add_argument('--scale_dir_init', type=str, default="", help='Scale dir that contains alphas and betas')
+    parser.add_argument('--scale_dir', type=str, default="", help='Scale dir that contains thetas and gammas')
     parser.add_argument('--scale_interp_step',type=int, default=-1)
-    # w_perturbated = w_inv + perturbation
-    parser.add_argument("--save_w_perturbated", action="store_true", help='Save final perturbated latent space')
-    parser.add_argument("--import_perturbation", action="store_true", help='Flag to import perturbation')
-    parser.add_argument('--initial_timestep',type=int, default =3, help='Initial timestep from which the perturbation is taken')
-    parser.add_argument('--dir_perturbation',type=str, default ="", help='Directory where perturbation were saved')
-    parser.add_argument("--save_perturbation", action="store_true", help='Directory where to save perturbation')
-    
-    parser.add_argument("--temporal_consistency", action="store_true", help='Whether to have a perturbation that is temporally consistent')
-    parser.add_argument("--sigma",type=float, default=0.1, help="volatility parameter for temporal consistency")
-    parser.add_argument("--theta",type=float, default=0.01, help="speed of mean reversion parameter for temporal consistency")
     
 
     ########################## CONTROL of Data to perturb ######################
@@ -246,8 +272,6 @@ if __name__=="__main__" :
     params = parser.parse_args()
     params.output_dir = params.output_dir + f"{params.sample_rule}_{params.style_indices}_{params.unbias}_{params.scale_interp_step}_{params.N_conditioners}_{params.add_name}/" 
 
-    if params.import_perturbation or params.temporal_consistency :
-        params.dir_perturbation = params.dir_perturbation + f"{params.sample_rule}_{params.style_indices}_{params.unbias}_{params.scale_interp_step}_{params.N_conditioners}_{params.add_name}/" 
     # create output directories
     if not os.path.exists(params.output_dir):
         os.makedirs(params.output_dir)
@@ -307,7 +331,6 @@ if __name__=="__main__" :
     
     metrics_list = ['variance', 'std_diff']#, 'mean_bias']
     metrics = {}
-    dt = None
 
     for date_ in liste_dates:
         datename = date_.strftime('%Y-%m-%d')
@@ -316,15 +339,11 @@ if __name__=="__main__" :
         for lt_id, lt in enumerate(params.leadtimes):    
 
             already_exist = []
-            if not params.import_perturbation :
-                label = 'generated_pert'
-            else :
-                label = 'imported_pert'
-            if os.path.isfile(params.output_dir + f'/samples/genFsemble_{params.date_index}_{lt}_{params.inv_step}_{params.N_conditioners}_{label}.npy'):
+            if os.path.isfile(params.output_dir + f'/samples/genFsemble_{params.date_index}_{lt}_{params.inv_step}_{params.N_conditioners}.npy'):
                 already_exist.append(True)
             else :
                 already_exist.append(False)
-            if os.path.isfile(params.output_dir + f'/samples/w_pert_{params.date_index}_{lt}_{params.inv_step}_{params.N_conditioners}_{label}.npy'):
+            if os.path.isfile(params.output_dir + f'/samples/w_pert_{params.date_index}_{lt}_{params.inv_step}_{params.N_conditioners}.npy'):
                 already_exist.append(True)
             else :
                 already_exist.append(False)
