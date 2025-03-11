c4f65fbcb7bc2b478f139fed2cdbe8cff9264029
sanchezvvictor <victor.sanchez@meteo.fr>
2025-03-11 17:39:02 +0100

    updates

--- inversion/psd_loss/spectral_loss.py
+++ inversion/psd_loss/spectral_loss.py
@@ -5,7 +5,6 @@
 from copy import deepcopy
 import matplotlib.pyplot as plt
 import numpy as np
-# from scipy.fftpack import dct, fft, idct
 from scipy.signal import welch, coherence
 import torch
 
@@ -17,27 +16,24 @@ class SpectralLoss(torch.nn.Module):
         super(SpectralLoss, self).__init__()
 
 
-    # def compute_MSE_loss_from_PSD_decomposition(self, x,y):
-    #     channels = x.shape[1]
-    #     loss = 0
-    #     for c in range(channels):
-    #         PSD_x = welch(x[:, c : c + 1, :, :])
-    #         PSD_y = welch(y[:, c : c + 1, :, :])
-    #         Coh = coherence(x[:, c : c + 1, :, :],y[:, c : c + 1, :, :])
-    #         loss += PSD_x + PSD_y - 2 * np.sqrt(PSD_x*PSD_y)*Coh
+    def compute_MSE_loss_from_PSD_decomposition(self, x,y):
+        channels = x.shape[1]
+        loss = 0
+        for c in range(channels):
+            PSD_x = welch(x[:, c : c + 1, :, :])
+            PSD_y = welch(y[:, c : c + 1, :, :])
+            Coh = coherence(x[:, c : c + 1, :, :],y[:, c : c + 1, :, :])
+            loss += PSD_x + PSD_y - 2 * np.sqrt(PSD_x*PSD_y)*Coh
 
-    #     return loss
+        return loss
 
     def compute_MSE_loss_from_PSD_adjusted_decomposition(self, x,y):
         channels = x.shape[1]
         loss = 0
         for c in range(channels):
             PSD_x = welch(x[:, c : c + 1, :, :].flatten(), fs=10000)[1]
-            # print('PSD_x.shape', PSD_x.shape)
             PSD_y = welch(y[:, c : c + 1, :, :].flatten(), fs=10000)[1]
-            # print('PSD_y.shape', PSD_y.shape)
             Coh = coherence(x[:, c : c + 1, :, :].flatten(),y[:, c : c + 1, :, :].flatten(), fs=10000)[1]
-            # print('Coh.shape', Coh.shape)
             spectral_loss = np.sum((np.sqrt(PSD_x) - np.sqrt(PSD_y))**2 + 2 * np.sqrt(PSD_x*PSD_y)*(1-Coh))
             loss += spectral_loss 
 
