a22b31b9c5f129a2d842da13adb1091bcd9f2f57
sanchezvvictor <victor.sanchez@meteo.fr>
2025-03-12 14:58:49 +0100

    updates

--- grandEnsemble/distance_between_members_grand_ensemble.py
+++ grandEnsemble/distance_between_members_grand_ensemble.py
@@ -29,19 +29,18 @@ if __name__=="__main__" :
     parser.add_argument('--ckpt_dir', type = str, default ='/project/home/p200177/DE_371/resources/models/trained_generator/000024.pt')
     # Real Data Directory - PATH to samples of the dataset
     
-    parser.add_argument('--pack_dir', type = str, default ='/project/home/p200177/DE_371/experiments_WP1/Grand_Ensemble/Final/Pack/')
+    parser.add_argument('--pack_dir', type = str, default ='/project/home/p200177/DE_371/experiments_WP1/Grand_Ensemble/Pack/')
     parser.add_argument('--real_data_dir', type = str, 
                         default='/project/home/p200177/DE_371/datasets/dataset_Meteo_France/IS_1_1.0_0_0_0_0_0_256_large_lt_done/')
     
     # Output Directory - PATH where the output of the inversion will be saved
-    parser.add_argument('--output_dir',type = str, default ='/project/home/p200177/DE_371/experiments_WP1/Grand_Ensemble/Scores/Distance_plots/')
+    parser.add_argument('--output_dir',type = str, default ='/project/home/p200177/DE_371/experiments_WP1/Grand_Ensemble/Scores/Optim/Distance_plots/')
     
-    parser.add_argument('--gen_sample_dir',type = str, default ="/project/home/p200177/DE_371/experiments_WP1/Grand_Ensemble/Final/Gen/")
+    parser.add_argument('--gen_data_dir',type = str, default ="/project/home/p200177/DE_371/experiments_WP1/Grand_Ensemble/Perturbation/Optim/stochastic_['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '0', '0', '0']_False/samples/")
 
     parser.add_argument("--var_indices", type=utils.str2intlist, default=[1,2,3])
     parser.add_argument("--Shape", type=tuple, default=(3,256,256), help='size of the samples')
     parser.add_argument("--crop_indices", type=int, nargs='+', default=[0,256,0,256])
-    
     
     # Dataset information
     parser.add_argument("--normalization", type=str, default="meanmax", choices=["minmax", "meanmax"])
@@ -54,6 +53,7 @@ if __name__=="__main__" :
     ########################## CONTROL of Data to invert ######################
     parser.add_argument("--leadtimes", type=utils.str2intlist, default=[6,12,18,24,30,36,42])
     
+    parser.add_argument('--inv_step', type=int, default=2000)
     parser.add_argument('--start_member', type=int, default=0)
     parser.add_argument('--stop_member', type=int, default=874)
 
@@ -81,8 +81,7 @@ if __name__=="__main__" :
        raise ValueError(f"Unknown normalization: {params.normalization}")
     
     # create output and pack directories
-    if not os.path.exists(params.output_dir):
-        os.makedirs(params.output_dir)
+    os.makedirs(params.output_dir, exist_ok=True)
 
 
     # set the seed for reproduciibility of runs
@@ -96,29 +95,50 @@ if __name__=="__main__" :
     #################### main loop ##################
     start = params.start_member
     stop = params.stop_member
-    random = True
+    random = False
     if random : 
         members_id = np.random.randint(0, 875, params.stop_member).tolist()
+
     for lt in params.leadtimes:
         path_to_pack_sample = params.pack_dir + f"Rsemble_{lt}_875.npy"
+        print(f'Start import of AROME Ensemble of leadtime {lt}')
+        
+        ens_r=utils.collate_R_ensemble(
+            data_dir=params.pack_dir,
+            members=list(range(start, stop)),
+            lead_time=lt,
+            var_indices=params.var_indices
+        )
+        
         if not random :
-            Ens_r = torch.from_numpy(np.load(path_to_pack_sample)).to(params.device)[:params.stop_member]
+            Ens_r = torch.from_numpy(ens_r).to(params.device)[:params.stop_member]
         else :
-            Ens_r = torch.from_numpy(np.load(path_to_pack_sample)).to(params.device)[members_id]
+            Ens_r = torch.from_numpy(ens_r).to(params.device)[members_id]
         
         
-        print('shape of AROME ensemble :', Ens_r.size())
+        print('Import done. Shape of AROME ensemble :', Ens_r.size())
         params.date_index = f'{start}_{stop}'
         params.lt_index = lt
         nb_member, _, _ ,_ = Ens_r.size()
 
         # try :
-        path_to_sample = params.gen_sample_dir + f"genFsemble_{lt}_875.npy"    
+        path_to_sample = params.gen_data_dir + f"genFsemble_{lt}_875.npy"   
+        print(f'Start import of Generated Ensemble of leadtime {lt}') 
+        gen_ens = utils.collate_gen_ensemble(
+            data_dir=params.gen_data_dir,
+            members=list(range(start, stop)),
+            lead_time=lt,
+            var_indices=params.var_indices,
+            inv_step=params.inv_step
+        )
+
         # Ens_gen = torch.from_numpy(shuffle_along_axis(np.load(path_to_sample), 0)).to(params.device)[:params.stop_member]
         if not random :
-            Ens_gen = torch.from_numpy(np.load(path_to_sample)).to(params.device)[:params.stop_member]
+            Ens_gen = torch.from_numpy(gen_ens).to(params.device)[:params.stop_member]
         else :
-            Ens_gen = torch.from_numpy(np.load(path_to_sample)).to(params.device)[members_id]
+            Ens_gen = torch.from_numpy(gen_ens).to(params.device)[members_id]
+
+        print('Import done. Shape of Generated ensemble :', Ens_gen.size())
 
         Ens_gen = normalize(
             data=Ens_gen.clone(),
@@ -140,6 +160,7 @@ if __name__=="__main__" :
         dist_ssim = np.zeros((nb_member+nb_gen_member, nb_member+nb_gen_member))
         pearson = np.ones((nb_member+nb_gen_member, nb_member+nb_gen_member))
         anomaly_correlation_coefficient = np.ones((nb_member+nb_gen_member, nb_member+nb_gen_member))
+        
         
         for i in range(nb_member+nb_gen_member):
             for j in range(i, nb_member+nb_gen_member):
@@ -210,6 +231,18 @@ if __name__=="__main__" :
         # dist_l2 = (dist_l2-dist_l2.min())/(dist_l2.max()-dist_l2.min())
         # dist_l1 = (dist_l1-dist_l1.min())/(dist_l1.max()-dist_l1.min())
         # dist_ssim = (dist_ssim-dist_ssim.min())/(dist_ssim.max()-dist_ssim.min())
+        output_dir_data = params.output_dir+'data/'
+        output_dir_plot = params.output_dir+'plots/'
+
+        os.makedirs(output_dir_data, exist_ok=True)
+        os.makedirs(output_dir_plot, exist_ok=True)
+
+        np.save(output_dir_data+f'dist_l2_between_member_{lt}_{nb_gen_member}.npy', dist_l2)
+        np.save(output_dir_data+f'dist_l1_between_member_{lt}_{nb_gen_member}.npy', dist_l1)
+        np.save(output_dir_data+f'dist_lssim_between_member_{lt}_{nb_gen_member}.npy', dist_ssim)
+        np.save(output_dir_data+f'pearson_between_member_{lt}_{nb_gen_member}.npy', pearson)
+        np.save(output_dir_data+f'acc_between_member_{lt}_{nb_gen_member}.npy', anomaly_correlation_coefficient)
+
 
         fig, ax = plt.subplots(nrows=1, ncols=5, figsize=(25,6))
         im=ax[0].matshow(dist_l2, cmap='plasma')
@@ -238,4 +271,7 @@ if __name__=="__main__" :
         ax[4].set_title('Anomaly Correlation Coefficient')
         fig.colorbar(im, ax=ax[4], shrink=0.5)
         fig.tight_layout()
-        fig.savefig(params.output_dir+f'distance_between_member_{lt}_{nb_gen_member}.png')
+        fig.savefig(output_dir_plot+f'distance_between_member_{lt}_{nb_gen_member}.png')
+
+        
+        
